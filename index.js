d9432e60f888e866b75b2177fd0275cb15c2b269
SChoolWab <schoolwab@gmail.com>
2019-03-22 14:33:50 +0600

    add item-03 Click to see Current time as alert and Chenge Html Dome

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ function item02() {
   document.getElementById('content02').innerHTML = 'Wow...! It is working.';
 }
 
-////////////     item-01     /////////////
+////////////     item-03     /////////////
+function item03_1() {
+  document.getElementById('content03').innerHTML = Date();
+}
+function item03_2() {
+  alert( Date() )
+}
 
 ////////////     item-01     /////////////
 
